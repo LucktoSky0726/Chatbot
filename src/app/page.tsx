97d7bee8a4ad5147b9a908e5673c4451a03d4f83
lucktosky726 <lucktosky@gmail.com>
2025-02-11 18:42:14 +0200

    fix: improve landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
       <main className="flex min-h-screen flex-col items-center  px-24 py-5">
         <Navbar/>
-        <section className="flex h-[50rem] w-[60rem] flex-col bg-gray-600 text-white rounded-xl p-10">
+        <section className="flex h-[50rem] w-[60rem] flex-col border-2 border-gray-600 rounded-xl p-10">
             <h2 className="text-2xl text-center">Welcome to My Page</h2>
             <div className="mt-2 p-2">This is a simple webpage using Tailwind CSS and Next.js to <b>earn competition score</b> by passing course that <b>GPT</b> made. </div>
             <div className='p-2'>
@@ -14,7 +14,7 @@ export default function Home() {
             </div>
             <div className='p-2'>
               <Link className='font-bold' href={"/langchain"}>Langchain</Link> is a Retrieval Augmented Generation APP. <br/>
-              You can ask some questions like these <b>How about GPT’s skill?</b>, <b>What did GPT say?</b>
+              You can ask some questions like these <b>How about GPT’s skill?</b>, <b>What did GPT said?</b>
             </div>
             <div className='p-2'>
               <Link className='font-bold' href={"/pdf"}>PDF</Link> is a PDFchat bot <br/>
